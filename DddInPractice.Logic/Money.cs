71eef2c1408c2fd86d0b5c35da52102e8f8f2bbf
imansafari1991 <imansafari33@gmail.com>
2021-12-18 00:57:23 +0330

    second commit

---
+++ DddInPractice.Logic/Money.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DddInPractice.Logic
+{
+    public sealed class Money:ValueObject<Money>
+    {
+        public int OneCentCount { get; set; }
+        public int TenCentCount { get; set; }
+        public int QuarterCount { get; set; }
+        public int OneDollarCount { get; set; }
+        public int FiveDollarCount { get; set; }
+        public int TwentyDollarCount { get; set; }
+
+        public Money(
+
+            int oneCentCount,
+            int tenCentCount,
+            int querterCount,
+            int oneDollarCount,
+            int fiveDollarCount,
+            int twentyDollarCount)
+        {
+
+            if (oneCentCount < 0) throw new InvalidOperationException();
+            if (tenCentCount < 0) throw new InvalidOperationException();
+            if (querterCount< 0) throw new InvalidOperationException();
+            if (oneDollarCount < 0) throw new InvalidOperationException();
+            if (fiveDollarCount < 0) throw new InvalidOperationException();
+            if (twentyDollarCount < 0) throw new InvalidOperationException();
+            OneCentCount = oneCentCount;
+            TenCentCount = tenCentCount;
+            QuarterCount = querterCount;
+            OneDollarCount = oneDollarCount;
+            FiveDollarCount = fiveDollarCount;
+            TwentyDollarCount = twentyDollarCount;
+
+        }
+
+        public decimal Amount =>
+            OneCentCount * 0.01m +
+            TenCentCount * 0.10m +
+            QuarterCount * 0.25m +
+            OneDollarCount +
+            FiveDollarCount * 5 +
+            TwentyDollarCount * 20;
+        public static Money operator +(Money money1, Money money2)
+        {
+            Money sum = new Money(money1.OneCentCount + money2.OneCentCount,
+                money1.TenCentCount + money2.TenCentCount,
+                money1.QuarterCount + money2.QuarterCount,
+                money1.OneDollarCount + money2.OneDollarCount,
+                money1.FiveDollarCount + money2.FiveDollarCount,
+                money1.TwentyDollarCount + money2.TwentyDollarCount);
+            return sum;
+
+        }
+        public static Money operator -(Money money1, Money money2)
+        {
+            Money subtract = new Money(money1.OneCentCount - money2.OneCentCount,
+                money1.TenCentCount - money2.TenCentCount,
+                money1.QuarterCount - money2.QuarterCount,
+                money1.OneDollarCount - money2.OneDollarCount,
+                money1.FiveDollarCount - money2.FiveDollarCount,
+                money1.TwentyDollarCount - money2.TwentyDollarCount);
+            return subtract;
+
+        }
+        protected override bool EqualsCore(Money other)
+        {
+            return OneCentCount == other.OneCentCount &&
+                 TenCentCount == other.TenCentCount &&
+                 QuarterCount == other.QuarterCount &&
+                 OneDollarCount == other.OneDollarCount &&
+                 FiveDollarCount == other.FiveDollarCount &&
+                 TwentyDollarCount == other.TwentyDollarCount;
+
+        }
+
+        protected override int GetHashCodeCore()
+        {
+            unchecked
+            {
+                int hashCode = OneCentCount;
+                hashCode = (hashCode * 397) ^ TenCentCount;
+                hashCode = (hashCode * 397) ^ QuarterCount;
+                hashCode = (hashCode * 397) ^ OneDollarCount;
+                hashCode = (hashCode * 397) ^ FiveDollarCount;
+                hashCode = (hashCode * 397) ^ TwentyDollarCount;
+                return hashCode;
+            }
+        }
+    } 
+}
---
+++ DddInPractice.Logic/SnackMachine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DddInPractice.Logic
+{
+    public class SnackMachine:Entity
+    {
+       public Money MoneyInside { get; private set; }
+       public Money MoneyInTransaction { get; private set; }
+
+     
+        public void InsertMoney(Money money)
+        {
+            MoneyInTransaction += money;
+
+        }
+        public void ReturnMoney()
+        {
+           //MoneyInTransaction=0
+
+        }
+        public void BuySnack()
+        {
+            MoneyInside += MoneyInTransaction;
+            //MoneyInTransaction = 0;
+        }
+
+    }
+}
